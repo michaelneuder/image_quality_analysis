f4d82e3b53ed2a699c0d4a30b2199c776c61af1d
Michael Neuder <michael.neuder@gmail.com>
2017-06-27 09:26:13 -0600

    new files and adding initializer calls

--- scripts/samples/debug.py
+++ scripts/samples/debug.py
@@ -9,7 +9,7 @@ import time
 def convolve_inner_layers(x, W, b):
     x = tf.nn.conv2d(x, W, strides = [1,1,1,1], padding='SAME')
     x = tf.nn.bias_add(x, b)
-    return tf.nn.sigmoid(x)
+    return tf.nn.tanh(x)
 
 def convolve_ouput_layer(x, W, b):
     x = tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
@@ -24,20 +24,32 @@ def conv_net(x, W, b):
     output = convolve_ouput_layer(conv4, W['weights_out'], b['bias_out'])
     return output
 
+def get_batch(x, y):
+    batch_indices = np.arange(x.shape[0])
+    np.random.shuffle(batch_indices)
+    x_batch = []
+    y_batch = []
+    for i in batch_indices[:50]:
+        x_batch.append(x[i])
+        y_batch.append(y[i])
+    return [x_batch, y_batch]
+
 def main():
+    print("--------------------------------------------------")
+    print("initializing variables ...")
     weights = {
-        'weights1': tf.Variable(tf.random_normal([11,11,2,30])),
-        'weights2': tf.Variable(tf.random_normal([11,11,30,15])),
-        'weights3': tf.Variable(tf.random_normal([11,11,15,7])),
-        'weights4': tf.Variable(tf.random_normal([11,11,7,3])),
-        'weights_out': tf.Variable(tf.random_normal([11,11,3,1]))
+        'weights1': tf.get_variable('weights1', shape=[11,11,2,30], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN')),
+        'weights2': tf.get_variable('weights2', shape=[11,11,30,15], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN')),
+        'weights3': tf.get_variable('weights3', shape=[11,11,15,7], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN')),
+        'weights4': tf.get_variable('weights4', shape=[11,11,7,3], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN')),
+        'weights_out': tf.get_variable('weights_out', shape=[11,11,3,1], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN'))
     }
     biases = {
-        'bias1': tf.Variable(tf.random_normal([30])),
-        'bias2': tf.Variable(tf.random_normal([15])),
-        'bias3': tf.Variable(tf.random_normal([7])),
-        'bias4': tf.Variable(tf.random_normal([3])),
-        'bias_out': tf.Variable(tf.random_normal([1]))
+        'bias1': tf.get_variable('bias1', shape=[30], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN')),
+        'bias2': tf.get_variable('bias2', shape=[15], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN')),
+        'bias3': tf.get_variable('bias3', shape=[7], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN')),
+        'bias4': tf.get_variable('bias4', shape=[3], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN')),
+        'bias_out': tf.get_variable('bias_out', shape=[1], initializer=tf.contrib.layers.variance_scaling_initializer(factor=1,mode='FAN_IN'))
     }
 
     # tf Graph input
@@ -45,12 +57,13 @@ def main():
     y = tf.placeholder(tf.float32, [None, 96, 96, 1])
 
     # data
-    original_images_train = np.loadtxt('../../data/sample_data/orig_3pics.txt')
-    reconstructed_images_train = np.loadtxt('../../data/sample_data/recon_3pics.txt')
-    comparison_images_train = np.loadtxt('../../data/sample_data/comp_3pics.txt')
-    original_images_test = np.loadtxt('../../data/sample_data/orig_3pics.txt')
-    reconstructed_images_test = np.loadtxt('../../data/sample_data/recon_3pics.txt')
-    comparison_images_test = np.loadtxt('../../data/sample_data/comp_3pics.txt')
+    print("loading data ...")
+    original_images_train = np.loadtxt('../../data/sample_data/orig_50.txt')
+    reconstructed_images_train = np.loadtxt('../../data/sample_data/recon_50.txt')
+    comparison_images_train = np.loadtxt('../../data/sample_data/comp_50.txt')
+    original_images_test = np.loadtxt('../../data/sample_data/orig_15.txt')
+    reconstructed_images_test = np.loadtxt('../../data/sample_data/recon_15.txt')
+    comparison_images_test = np.loadtxt('../../data/sample_data/comp_15.txt')
 
     # get size of training and testing set
     train_size = original_images_train.shape[0]
@@ -75,7 +88,7 @@ def main():
     input_combined_test = np.reshape(input_combined_test, [test_size, 96,96, 2])
 
     # paramaters
-    learning_rate = .01
+    learning_rate = .0001
     training_iterations = 10000
 
     # model
@@ -93,12 +106,11 @@ def main():
         sess.run(init)
         step = 1
         start_time = time.time()
-        print("--------------------------------------------------")
         print("starting training ... ")
         while step < training_iterations:
-            # batch = get_batch(input_combined_train, comparison_images_train)
-            # x_data_train , y_data_train = np.asarray(batch[0]), np.asarray(batch[1])
-            x_data_train, y_data_train = [input_combined_train[0]], [comparison_images_train[0]]
+            batch = get_batch(input_combined_train, comparison_images_train)
+            x_data_train , y_data_train = np.asarray(batch[0]), np.asarray(batch[1])
+            # x_data_train, y_data_train = [input_combined_train[0]], [comparison_images_train[0]]
             sess.run(optimizer, feed_dict={x : x_data_train, y : y_data_train})
             loss = sess.run(error, feed_dict={x : x_data_train, y : y_data_train})
             print("training step {}. current error: {}. ".format(step, loss))
@@ -106,8 +118,8 @@ def main():
         print("optimization finished!")
         print("--------------------------------------------------")
         print("testing accuracy")
-        # x_data_test, y_data_test = input_combined_test.reshape([test_size,96,96,2]), comparison_images_test
-        x_data_test, y_data_test = [input_combined_test[0]], [comparison_images_test[0]]
+        x_data_test, y_data_test = input_combined_test.reshape([test_size,96,96,2]), comparison_images_test
+        # x_data_test, y_data_test = [input_combined_test[0]], [comparison_images_test[0]]
         final_error = sess.run(error, feed_dict={x: x_data_test, y: y_data_test})
         print("the average pixel difference on the test set is {}.".format(final_error))
         print("--------------------------------------------------")
--- scripts/samples/image_viewer.py
+++ scripts/samples/image_viewer.py
@@ -6,15 +6,18 @@ def main():
     '''
     just to look at a couple of the images.
     '''
-    original_images = np.loadtxt("../../data/sample_data/orig_3pics.txt")
-    reconstructed_images = np.loadtxt("../../data/sample_data/recon_3pics.txt")
-    original_images = original_images.reshape(3,96,96)
-    reconstructed_images = reconstructed_images.reshape(3,96,96)
+    original_images = np.loadtxt("../../data/sample_data/orig_15.txt")
+    reconstructed_images = np.loadtxt("../../data/sample_data/recon_15.txt")
+    original_images = original_images.reshape(15,96,96)
+    reconstructed_images = reconstructed_images.reshape(15,96,96)
 
-    image = np.asarray(original_images[0], dtype='uint8')
-    # image = np.asarray(reconstructed_images[1], dtype='uint8')
-    image_view = im.fromarray(image, 'L')
+    image1 = np.asarray(original_images[14], dtype='uint8')
+    image2 = np.asarray(reconstructed_images[14], dtype='uint8')
+    image_view = im.fromarray(image1, 'L')
     image_view.show()
+    image_view = im.fromarray(image2, 'L')
+    image_view.show()
+
 
 if __name__ == '__main__':
     main()
