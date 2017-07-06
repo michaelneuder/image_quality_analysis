7fb97f1973c5e5e538a6a3f597f383875736e66b
Michael Neuder <michael.neuder@gmail.com>
2017-07-06 14:51:38 -0600

    we rrrr rolling folks

--- scripts/data_analysis/prediction_target_plotter.py
+++ scripts/data_analysis/prediction_target_plotter.py
@@ -9,23 +9,17 @@ import scipy.stats as stats
 def main():
     prediction_plot = []
     target_plot = []
-    with open('../../data/results/net_output/post_training.csv', mode='r') as csv_file:
+    with open('../nets/wip/post_training.csv', mode='r') as csv_file:
         reader = csv.reader(csv_file)
         line_count = 0
         for line in reader:
             if line_count > 0:
-                prediction_plot.append(float(line[0]))
-                target_plot.append(float(line[1]))
+                prediction_plot.append(float(line[1]))
+                target_plot.append(float(line[0]))
             line_count+=1
     csv_file.close()
     prediction_plot = np.asarray(prediction_plot)
     target_plot = np.asarray(target_plot)
-    # with open('../nets/wip/results/post_training_target.csv', mode='r') as csv_file:
-    #     reader = csv.reader(csv_file)
-    #     for line in reader:
-    #         target_plot.append(float(line[0]))
-    # csv_file.close()
-    # target_plot = np.asarray(target_plot)
 
     slope, intercept, r_value, p_value_, std_err = stats.linregress(prediction_plot, target_plot)
 
@@ -33,7 +27,7 @@ def main():
     plt.title('prediction vs target')
     plt.xlabel('prediction')
     plt.ylabel('target')
-    plt.text(0.7, -0.3, 'r-squared = {0:.5f}'.format(r_value**2), style='italic',
+    plt.text(0.12, .9, 'r-squared = {0:.5f}'.format(r_value**2), style='italic',
         bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
     plt.show()
 
@@ -48,6 +42,18 @@ def main():
     differences_squared = np.asarray(differences_squared)
     print(differences.mean(), differences_squared.mean())
 
+    x_plot = np.arange(len(target_plot))
+    plt.plot(x_plot, prediction_plot, 'r+')
+    plt.xlabel('pixel')
+    plt.ylabel('prediction')
+    plt.show()
+
+    n, bins, patches = plt.hist(prediction_plot, 50, normed=1, facecolor='blue', alpha=0.5, label='prediction')
+    n, bins, patches = plt.hist(target_plot, 50, normed=1, facecolor='red', alpha=0.5, label='target')
+    plt.xlabel('difference')
+    plt.ylabel('quantity')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()
--- scripts/nets/wip/pixel_diff_conv_net.py
+++ scripts/nets/wip/pixel_diff_conv_net.py
@@ -6,25 +6,32 @@ np.set_printoptions(threshold=np.nan)
 import tensorflow as tf
 import time
 
+def convolve_ouput_layer(x, W, b):
+    y = tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+    y = tf.nn.bias_add(y, b)
+    return tf.nn.relu(y)
+
 def conv_net(x, W, b):
-    y = tf.nn.conv2d(x, W['weights_out'], strides=[1,1,1,1], padding='SAME')
-    y = tf.nn.bias_add(y, b['bias_out'])
-    return y
+    output = convolve_ouput_layer(x, W['weights_out'], b['bias_out'])
+    return output
 
 def main():
     print('generating random images ... ')
     rand_img_1 = np.random.random_sample((96,96))
     rand_img_2 = np.random.random_sample((96,96))
-    difference = abs(rand_img_1 - rand_img_2)
+    difference = rand_img_1 - rand_img_2
+    sums = rand_img_1 + rand_img_2
 
-    combined_image_data = np.reshape(np.dstack((rand_img_1, rand_img_2)), [1,96,96,2])
-    difference_image_data = np.reshape(difference, [1,96,96,1])
+    train_data = np.reshape(np.dstack((rand_img_1, rand_img_2)), [1,96,96,2])
+    # target_data = np.reshape(difference, [1,96,96,1])
+    # target_data = np.reshape(rand_img_2, [1,96,96,1])
+    target_data = np.reshape(sums, [1,96,96,1])
 
     weights = {
-        'weights_out': tf.Variable((1/(11*11))*tf.random_normal([11,11,2,1]))
+        'weights_out': tf.Variable((1/(10*11*11))*tf.random_normal([11,11,2,1]))
     }
     biases = {
-        'bias_out': tf.Variable((1/(11*11))*tf.random_normal([1]))
+        'bias_out': tf.Variable((1/(10*11*11))*tf.random_normal([1]))
     }
 
     # tf Graph input
@@ -32,8 +39,8 @@ def main():
     y = tf.placeholder(tf.float32, [None, 96, 96, 1])
 
     # paramaters
-    learning_rate = .01
-    epochs = 1000
+    learning_rate = .001
+    epochs = 2500
 
     # model
     prediction = conv_net(x, weights, biases)
@@ -46,30 +53,26 @@ def main():
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-        global_step = 0
         epoch_count = 0
         start_time = time.time()
         print("starting training ... ")
         while epoch_count < epochs:
-            x_data_train, y_data_train = combined_image_data, difference_image_data
+            x_data_train, y_data_train = train_data, target_data
             sess.run(optimizer, feed_dict={x : x_data_train, y : y_data_train})
             loss = sess.run(cost, feed_dict={x : x_data_train, y : y_data_train})
-            print("  -  training global_step {}. current error: {}. ".format(global_step, loss))
-            global_step+=1
+            print("  -  training global_step {}. current error: {}. ".format(epoch_count, loss))
             epoch_count+=1
         print('optimization finished!')
-        #
-        # prediction = np.asarray(sess.run(prediction, feed_dict={x : [combined_data_train[0]]}))
-        # target = np.asarray([comparison_images_test[0]])
-        # print(prediction.shape, target.shape)
-        # with open('post_training.csv', mode = 'w') as write_file:
-        #     write_file.write('target, prediction\n')
-        #     for i in range(96):
-        #         for j in range(96):
-        #             write_file.write(str(float(target[0][i][j][0])) + ', ' + str(float(prediction[0][i][j][0])) + '\n')
-        # write_file.close()
 
-
+        pred = np.asarray(sess.run(prediction, feed_dict={x : train_data}))
+        target = np.asarray(target_data)
+        print(pred.shape, target.shape)
+        with open('post_training.csv', mode = 'w') as write_file:
+            write_file.write('target, prediction\n')
+            for i in range(96):
+                for j in range(96):
+                    write_file.write(str(float(target[0][i][j][0])) + ', ' + str(float(pred[0][i][j][0])) + '\n')
+        write_file.close()
 
 if __name__ == '__main__':
     main()
