1a3f6eeed44417525d183599839a9f8ac39f0bdd
michaelneuder <michael.neuder@gmail.com>
2017-09-19 15:37:37 -0600

    rearrange

--- bin/nets/wip/SSIM_conv_net.py
+++ bin/nets/wip/SSIM_conv_net.py
@@ -5,6 +5,8 @@ import numpy as np
 np.set_printoptions(threshold=np.nan)
 import tensorflow as tf
 import time
+import progressbar
+import pandas as pd
 
 def convolve_inner_layers(x, W, b):
     y = tf.nn.conv2d(x, W, strides = [1,1,1,1], padding='SAME')
@@ -20,7 +22,8 @@ def conv_net(x, W, b):
     conv1 = convolve_inner_layers(x, W['weights1'], b['bias1'])
     conv2 = convolve_inner_layers(conv1, W['weights2'], b['bias2'])
     conv3 = convolve_inner_layers(conv2, W['weights3'], b['bias3'])
-    output = convolve_ouput_layer(conv3, W['weights_out'], b['bias_out'])
+    output_feed = tf.concat([conv1, conv2, conv3],3)
+    output = convolve_ouput_layer(output_feed, W['weights_out'], b['bias_out'])
     return output
 
 def get_variance(training_target):
@@ -55,53 +58,57 @@ def get_epoch(x, y, n):
 def main():
     # parameters
     filter_dim = 11
-    filter_dim2 = 11
-    number_images = 100
+    filter_dim2 = 1
     batch_size = 4
     image_dim = 96
     input_layer = 2
-    first_layer = 50
-    second_layer = 25
-    third_layer = 10
+    first_layer = 25
+    second_layer = 10
+    third_layer = 5
     output_layer = 1
     initializer_scale = 1.0
+    learning_rate = .00001
+    epochs = 200
 
     # seeding for debug purposes --- dont forget to remove
     SEED = 12345
     np.random.seed(SEED)
     tf.set_random_seed(SEED)
 
-    print('generating random images ... ')
-    # train images
-    rand_img_train_1_raw = np.random.random_sample((number_images,image_dim**2))
-    rand_img_train_2_raw = np.random.random_sample((number_images,image_dim**2))
-    mean1, std_dev1 = rand_img_train_1_raw.mean(), rand_img_train_1_raw.std()
-    mean2, std_dev2 = rand_img_train_2_raw.mean(), rand_img_train_2_raw.std()
-    rand_img_train_1 = (rand_img_train_1_raw - mean1) / std_dev1
-    rand_img_train_2 = (rand_img_train_2_raw - mean2) / std_dev2
-    difference_train = abs(rand_img_train_1 - rand_img_train_2)
+    print('loading image files ... ')
+    # train/test images
+    orig_500 = pd.read_csv('https://raw.githubusercontent.com/michaelneuder/image_quality_analysis/master/data/sample_data/orig_500.txt', header=None, delim_whitespace = True)
+    original_images_train = orig_500.values
+    recon_500 = pd.read_csv('https://raw.githubusercontent.com/michaelneuder/image_quality_analysis/master/data/sample_data/recon_500.txt', header=None, delim_whitespace = True)
+    reconstructed_images_train = recon_500.values
+    SSIM_500 = pd.read_csv('https://raw.githubusercontent.com/michaelneuder/image_quality_analysis/master/data/sample_data/SSIM_500.txt', header=None, delim_whitespace = True)
+    comparison_images_train = SSIM_500.values
 
-    # test image
-    rand_img_test_1_raw = np.random.random_sample((number_images,image_dim**2))
-    rand_img_test_2_raw = np.random.random_sample((number_images,image_dim**2))
-    mean1, std_dev1 = rand_img_test_1_raw.mean(), rand_img_test_1_raw.std()
-    mean2, std_dev2 = rand_img_test_2_raw.mean(), rand_img_test_2_raw.std()
-    rand_img_test_1 = (rand_img_test_1_raw - mean1) / std_dev1
-    rand_img_test_2 = (rand_img_test_2_raw - mean2) / std_dev2
-    difference_test = abs(rand_img_test_1 - rand_img_test_2)
+    orig_140 = pd.read_csv('https://raw.githubusercontent.com/michaelneuder/image_quality_analysis/master/data/sample_data/orig_140.txt', header=None, delim_whitespace = True)
+    original_images_test = orig_140.values
+    recon_140 = pd.read_csv('https://raw.githubusercontent.com/michaelneuder/image_quality_analysis/master/data/sample_data/recon_140.txt', header=None, delim_whitespace = True)
+    reconstructed_images_test = recon_140.values
+    SSIM_140 = pd.read_csv('https://raw.githubusercontent.com/michaelneuder/image_quality_analysis/master/data/sample_data/SSIM_140.txt', header=None, delim_whitespace = True)
+    comparison_images_test = SSIM_140.values
 
-    # stacking & reshaping images
-    train_data = np.reshape(np.dstack((rand_img_train_1, rand_img_train_2)), [number_images,image_dim,image_dim,2])
-    test_data = np.reshape(np.dstack((rand_img_test_1, rand_img_test_2)), [number_images,image_dim,image_dim,2])
-    target_data_train = np.reshape(difference_train, [number_images,image_dim,image_dim,1])
-    target_data_test = np.reshape(difference_test, [number_images,image_dim,image_dim,1])
+    # get size of training and testing set
+    train_size = original_images_train.shape[0]
+    test_size = original_images_test.shape[0]
+
+    # reshaping the result data to --- (num pics), 96, 96, 1
+    target_data_train = np.reshape(comparison_images_train, [train_size, image_dim, image_dim, 1])
+    target_data_test = np.reshape(comparison_images_test, [test_size, image_dim, image_dim, 1])
+
+    # zipping data
+    train_data = np.reshape(np.dstack((original_images_train, reconstructed_images_train)), [train_size,image_dim,image_dim,2])
+    test_data =  np.reshape(np.dstack((original_images_test, reconstructed_images_test)), [test_size,image_dim,image_dim,2])
 
     # initializing variables --- fan in
     weights = {
         'weights1': tf.Variable(tf.random_normal([filter_dim,filter_dim,input_layer,first_layer],stddev=(1.0/(initializer_scale*filter_dim*filter_dim*input_layer)))),
         'weights2': tf.Variable(tf.random_normal([filter_dim2,filter_dim2,first_layer,second_layer],stddev=(1.0/(initializer_scale*filter_dim2*filter_dim2*first_layer)))),
         'weights3': tf.Variable(tf.random_normal([filter_dim2,filter_dim2,second_layer,third_layer],stddev=(1.0/(initializer_scale*filter_dim2*filter_dim2*second_layer)))),
-        'weights_out': tf.Variable(tf.random_normal([filter_dim2,filter_dim2,third_layer,output_layer],stddev=(1.0/(initializer_scale*filter_dim2*filter_dim2*third_layer))))
+        'weights_out': tf.Variable(tf.random_normal([filter_dim2,filter_dim2,third_layer+second_layer+first_layer,output_layer],stddev=(1.0/(initializer_scale*filter_dim2*filter_dim2*third_layer))))
     }
     biases = {
         'bias1': tf.Variable(tf.random_normal([first_layer],stddev=(1.0/(initializer_scale*filter_dim*filter_dim*input_layer)))),
@@ -114,15 +121,11 @@ def main():
     x = tf.placeholder(tf.float32, [None, image_dim, image_dim, 2])
     y = tf.placeholder(tf.float32, [None, image_dim, image_dim, 1])
 
-    # paramaters
-    learning_rate = .01
-    epochs = 1000
-
     # model
     prediction = conv_net(x, weights, biases)
 
     # get variance to normalize error terms during training
-    variance = get_variance(difference_train)
+    variance = get_variance(target_data_train)
 
     # loss and optimization
     cost = tf.reduce_mean(tf.square(tf.subtract(prediction, y)))
@@ -136,12 +139,11 @@ def main():
         global_step = 0
         start_time = time.time()
         print("starting training ... ")
-        while epoch_count < epochs:
-            epoch_time = time.time()
-            print('--------------------------------------------------------------')
+        for j in range(epochs):
+            print('---------------------------------------------------------')
             print('beginning epoch {} ...'.format(epoch_count))
             epoch = get_epoch(train_data, target_data_train, batch_size)
-            for i in epoch:
+            for i in range(len(epoch)):
                 x_data_train, y_data_train = np.asarray(epoch[i][0]), np.asarray(epoch[i][1])
                 sess.run(optimizer, feed_dict={x : x_data_train, y : y_data_train})
                 loss = sess.run(cost, feed_dict={x : x_data_train, y : y_data_train})
@@ -149,14 +151,12 @@ def main():
                 print("  -  training global_step {0:4d} error: {1:8.4f} {2:8.2f}%".format(global_step, loss, percent_error))
                 global_step += 1
             epoch_count+=1
-            print('epoch finished in {0:5.1f} seconds. total time training: {1:5.1f} seconds'.format(abs(epoch_time-time.time()), abs(start_time-time.time())))
         print('optimization finished!')
         print('\nstarting testing...')
         score = sess.run(cost, feed_dict={x: test_data, y: target_data_test})
+        percent_error = 100*score/variance
         pred = sess.run(prediction, feed_dict={x: test_data})
-        for i in range(image_dim):
-            print(rand_img_test_1[0][i],rand_img_test_2[0][i], pred[0][0][i], difference_test[0][i])
-        print('---- score : {} ----'.format(score))
+        print('---- test score : {:.4f}, {:.4f}% ----'.format(score, percent_error))
 
 if __name__ == '__main__':
     main()
