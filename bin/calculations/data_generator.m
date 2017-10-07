7d20c00943f536bb00165c5f09ec1ff7d3565043
michaelneuder <michael.neuder@gmail.com>
2017-10-07 12:54:13 -0600

    new data data data

--- bin/calculations/data_generator.m
+++ bin/calculations/data_generator.m
@@ -1,26 +1,26 @@
 clear all;
 
 % file read
-orig = dlmread('/home/dirty_mike/Dropbox/github/image_quality_analysis/data/sample_data/orig_500.txt', ' ');
-recon = dlmread('/home/dirty_mike/Dropbox/github/image_quality_analysis/data/sample_data/recon_500.txt', ' ');
+orig = dlmread('/home/dirty_mike/Dropbox/github/image_quality_analysis/data/sample_data/orig_140.txt', ' ');
+recon = dlmread('/home/dirty_mike/Dropbox/github/image_quality_analysis/data/sample_data/recon_140.txt', ' ');
 
 % slice extra column
-orig = orig(1:500,1:9216);
-recon = recon(1:500,1:9216);
+orig = orig(1:140,1:9216);
+recon = recon(1:140,1:9216);
 
 % initialize
 image_dim = 96;
-ssim = zeros(500,86,86);
+ssim = zeros(140,86,86);
 K = [0.01 0.03];
 window = ones(11);
 L = 1255;
 
 % reshape array
-orig = reshape(orig,[500, image_dim, image_dim]);
-recon = reshape(recon,[500, image_dim, image_dim]);
+orig = reshape(orig,[140, image_dim, image_dim]);
+recon = reshape(recon,[140, image_dim, image_dim]);
 
 % loop and call
-for i=1:500
+for i=1:140
     orig_temp = reshape(orig(i,:,:),[image_dim, image_dim]);
     recon_temp = reshape(recon(i,:,:),[image_dim, image_dim]);
     [mssim, ssim_map] = ssim_index(orig_temp, recon_temp);
